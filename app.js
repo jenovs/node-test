fe503b653360c923ccfd9ca1d3f7309b1f64b800
Viktors Jenovs <jviktors@gmail.com>
2021-02-13 15:38:49 +0200

    Add env endpoint

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
   res.send(`<h1 style="font-size: 3rem;">${num}</h1>`);
 });
 
+app.get('/env', (req, res) => {
+  res.send(process.env.TEST_ENV_VARIABLE);
+});
+
 app.listen(8080);
