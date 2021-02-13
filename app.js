6e00923043dcc2e358e51c4004ea683e9f65f7da
Viktors Jenovs <jviktors@gmail.com>
2021-02-13 12:44:05 +0200

    Add counter

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+let count = 0;
+
 app.get('/', (req, res) => {
-  res.send(`<h1 style="font-size: 3rem;">${Math.random()}</h1>`);
+  const num = Math.random();
+  count++;
+
+  res.send(
+    `<h1 style="font-size: 3rem;">${num}</h1><p>Request count: ${count}</p>`
+  );
 });
 
 app.listen(8080);
